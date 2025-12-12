dd77387239f9685143266fa977ca06b4af22286a
Filip Gavrilovski <filipgavrilovski28@gmail.com>
2025-12-12 08:22:50 +0100

    add day 12

--- aoc_25/day12/Day12.java
+++ aoc_25/day12/Day12.java
@@ -2,29 +2,60 @@ package aoc_25.day12;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 class Helper{}
 
+record Region(long area, ArrayList<Long> giftCounts) {}
+
 class Part1{
     public static long solve(ArrayList<String> inputs){
+        ArrayList<Long> areas = new ArrayList<>();
+        ArrayList<Region> targets = new ArrayList<>();
+        long hashtagCount = 0;
+        int index = 0;
+        for (String line : inputs.stream().skip(1).toList()){
+            if (line.equals(String.format("%d:", index + 1))) {
+                areas.add(index++, hashtagCount);
+                hashtagCount = 0;
+            }
+            else if (!line.contains(":")) {
+                hashtagCount += line.chars().mapToLong(c -> c == '#' ? 1 : 0).sum();
+            }
+            else {
+                if (index != -1) {
+                    areas.add(index, hashtagCount);
+                    index = -1;
+                }
+                long height = Long.parseLong(line.split("x")[0]);
+                long width = Long.parseLong(line.split("x")[1].split(":")[0]);
+                long[] array = Arrays.stream(line.split(": ")[1].split("\\s+"))
+                        .mapToLong(Long::parseLong)
+                        .toArray();
+                ArrayList<Long> al = new ArrayList<>();
+                for (long n : array) al.add(n);
+                targets.add(new Region(height * width, al));
+            }
+        }
         long ans = 0;
+        for (Region ad: targets){
+            ArrayList<Long> counts = ad.giftCounts();
+            long sum = 0;
+            for (int i = 0; i < counts.size(); i++) {
+                sum += counts.get(i) * areas.get(i);
+            }
+            if (ad.area() >= sum) ans++;
+        }
+
         return ans;
     }
 }
 
-class Part2{
-    public static long solve(ArrayList<String> inputs){
-        long ans = 0;
-        return ans;
-    }
-}
 
 public class Day12 {
     public static void main(String[] args) throws FileNotFoundException {
-//        File file = new File("aoc_25/day12/input.txt");
-        File file = new File("aoc_25/day12/test-input.txt");
+        File file = new File("aoc_25/day12/input.txt");
+//        File file = new File("aoc_25/day12/test-input.txt");
 
         Scanner sc = new Scanner(file);
 
@@ -34,7 +65,6 @@ public class Day12 {
         }
 
         System.out.println("part1: " + Part1.solve(inputs));
-        System.out.println("part2: " + Part2.solve(inputs));
     }
 }
 
